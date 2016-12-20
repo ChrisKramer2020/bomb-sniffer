67609f616c2fbb84fa7b7b79f3cf26444c3b6784
Christopher Kramer <Hendrix1957@gmail.com>
2016-12-20 13:05:51 -0500

    my work

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,32 @@ import React, { Component } from 'react'
 class App extends Component {
 
   render () {
-    return <div>
-      <h1>Hello, World!</h1>
+    return <div className='app'>
+      <h1>Explosion Avoider!</h1>
+      <div className='gameboard'>
+        <table>
+          <tbody>
+            <tr>
+              <td className='empty' />
+              <td className='numbered'> 3 </td>
+              <td className='flagged' />
+            </tr>
+            <tr>
+              <td className='bomb' />
+              <td className='unrevealed' />
+              <td className='flagbomb' />
+            </tr>
+            <tr>
+              <td className='unrevealed' />
+              <td className='unrevealed' />
+              <td className='unrevealed' />
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <footer>
+        Made with &hearts; At the Iron Yard
+      </footer>
     </div>
   }
 }
